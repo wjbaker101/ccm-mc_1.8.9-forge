5f7d429a9b7bcfd4e2eea46ef37f4233be0cf179
wjbaker101 <willbaker10198@gmail.com>
2023-02-18 17:08:30 +0000

    Add Mixins, in order to prevent the debug crosshair from appearing under the custom crosshair the private method in EntityRenderer needs to be cancelled. This can only easily be done through Mixins

---
+++ src/main/java/com/wjbaker/ccm/mixins/MixinLoader.java
@@ -0,0 +1,39 @@
+package com.wjbaker.ccm.mixins;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
+
+import java.util.Map;
+
+public final class MixinLoader implements IFMLLoadingPlugin {
+
+    public MixinLoader() {
+        MixinBootstrap.init();
+        Mixins.addConfiguration("mixins.ccm.json");
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(final Map<String, Object> data) {
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
