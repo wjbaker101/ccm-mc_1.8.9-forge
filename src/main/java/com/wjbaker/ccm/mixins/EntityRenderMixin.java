ff650dbcb82f3537be833ab0cb8861b37d82b898
wjbaker101 <willbaker10198@gmail.com>
2023-02-19 01:05:00 +0000

    Add logic for debug style
    Instead of manually rendering it, override the method and change up the if statements. It uses lots of private methods/properties so unable to manually render

--- src/main/java/com/wjbaker/ccm/mixins/EntityRenderMixin.java
+++ src/main/java/com/wjbaker/ccm/mixins/EntityRenderMixin.java
@@ -1,24 +1,56 @@
 package com.wjbaker.ccm.mixins;
 
 import com.wjbaker.ccm.CustomCrosshairMod;
+import com.wjbaker.ccm.crosshair.style.CrosshairStyle;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderGlobal;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.AxisAlignedBB;
+import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(EntityRenderer.class)
 public class EntityRenderMixin {
 
-    @Inject(
-        at = @At("HEAD"),
-        method = "renderWorldDirections",
-        cancellable = true
-    )
-    private void renderWorldDirections(final CallbackInfo info) {
-        if (!CustomCrosshairMod.INSTANCE.properties().getIsModEnabled().get())
+    /**
+     * @author Sparkless101
+     */
+    @Overwrite
+    private void renderWorldDirections(final float partialTicks) {
+        Minecraft mc = Minecraft.getMinecraft();
+
+        if (mc.gameSettings.hideGUI || mc.thePlayer.hasReducedDebug() || mc.thePlayer.hasReducedDebug())
             return;
 
-        info.cancel();
+        boolean isDebugStyle = CustomCrosshairMod.INSTANCE.properties().getCrosshair().style.get() == CrosshairStyle.DEBUG;
+
+        if (!isDebugStyle)
+            return;
+
+        Entity entity = mc.getRenderViewEntity();
+        GlStateManager.enableBlend();
+        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+        GL11.glLineWidth(1.0F);
+        GlStateManager.disableTexture2D();
+        GlStateManager.depthMask(false);
+        GlStateManager.pushMatrix();
+        GlStateManager.matrixMode(5888);
+        GlStateManager.loadIdentity();
+        this.orientCamera(partialTicks);
+        GlStateManager.translate(0.0F, entity.getEyeHeight(), 0.0F);
+        RenderGlobal.drawOutlinedBoundingBox(new AxisAlignedBB(0.0D, 0.0D, 0.0D, 0.005D, 1.0E-4D, 1.0E-4D), 255, 0, 0, 255);
+        RenderGlobal.drawOutlinedBoundingBox(new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0E-4D, 1.0E-4D, 0.005D), 0, 0, 255, 255);
+        RenderGlobal.drawOutlinedBoundingBox(new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0E-4D, 0.0033D, 1.0E-4D), 0, 255, 0, 255);
+        GlStateManager.popMatrix();
+        GlStateManager.depthMask(true);
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
     }
+
+    @Shadow
+    private void orientCamera(float partialTicks) {}
 }
